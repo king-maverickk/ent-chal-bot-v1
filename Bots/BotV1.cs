79677a4e212af05ce9e04bbd51676a40b53ad5b7
king-maverickk <maverick.motheo.iv@gmail.com>
2024-06-29 14:07:08 +0200

    feat(Bots): check to see if Bot is within bounds

---
+++ Bots/BotV1.cs
@@ -0,0 +1,43 @@
+﻿using ent_chal_bot_v1.Enums;
+using ent_chal_bot_v1.Models;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ent_chal_bot_v1.Bots
+{
+    public class BotV1
+    {
+        private readonly BotStateDTO _botState;
+        private readonly HubConnection _connection;
+
+        public BotV1(BotStateDTO botState, HubConnection connection)
+        {
+            _botState = botState;
+            _connection = connection;
+        }
+
+        // check if bot is within the grid bounds
+        public bool IsWithinBounds(InputCommand direction)
+        {
+            int newX = _botState.X;
+            int newY = _botState.Y;
+
+            switch (direction)
+            {
+                case InputCommand.LEFT:
+                    newX--;
+                    break;
+                case InputCommand.RIGHT:
+                    newX++;
+                    break;
+                case InputCommand.DOWN:
+                    newY++;
+                    break;
+                case InputCommand.UP:
+                    newY--;
+                    break;
+            }
+            // Check if new position is within the 50x50 bounds
+            return newX >= 0 && newX < 50 && newY >= 0 && newY < 50;
+        }
+    }
+}

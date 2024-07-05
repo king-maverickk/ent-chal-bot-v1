a8fa579e598c835b2715a2d2ef10f2b961fbfe69
king-maverickk <maverick.motheo.iv@gmail.com>
2024-07-05 11:19:45 +0200

    feat: is bot on territory edge

--- TrianglePlan.cs
+++ TrianglePlan.cs
@@ -9,7 +9,30 @@ namespace ent_chal_bot_v1
 {
     internal class TrianglePlan
     {
+        public bool IsOnTerritoryEdge(BotStateDTO botState)
+        {
+            int[][] view = botState.HeroWindow;
 
+            // Check the four adjacent cells
+            if (view[3][4] == 255) // Left
+            {
+                return true;
+            }
+            if (view[5][4] == 255) // Right
+            {
+                return true;
+            }
+            if (view[4][3] == 255) // Down
+            {
+                return true;
+            }
+            if (view[4][5] == 255) // Up
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 
 }

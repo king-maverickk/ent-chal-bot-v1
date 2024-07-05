92cf2d3fdc9f1a4aac6f7d99f395951f8c23f261
king-maverickk <maverick.motheo.iv@gmail.com>
2024-07-05 11:18:52 +0200

    add: empty class file for triangle

---
+++ TrianglePlan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ent_chal_bot_v1.Models;
+
+namespace ent_chal_bot_v1
+{
+    internal class TrianglePlan
+    {
+
+    }
+
+}

e37968eef46c6904f49fef0628de19cbda2210c3
king-maverickk <maverick.motheo.iv@gmail.com>
2024-07-15 15:11:10 +0200

    add the NEATBot cs file

---
+++ Bots/NEATBot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ent_chal_bot_v1.Bots
+{
+    public class NEATBot
+    {
+
+    }
+}
